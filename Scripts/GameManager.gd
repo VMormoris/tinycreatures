13810ca4c91fce966ba0c81c84397bdcd1d5c486
MiguelCR <miguelcroman11@gmail.com>
2024-10-06 23:23:05 +0200

    Day difficulty increase

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -16,7 +16,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if date >= 3:
+		var timer = $SlimeTimer
+		timer.wait_time = 2.5
 
 
 func _on_slime_timer_timeout() -> void:
