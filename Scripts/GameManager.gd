488b14259a00a4e55615de4cb8778d3f58112163
MiguelCR <miguelcroman11@gmail.com>
2024-10-06 18:57:40 +0200

    Spawn around the player

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 @export var slime_scene: PackedScene
+@export var enemies: int = 50
+@export var range: int = 200
+
+@onready var player: CharacterBody2D = %Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,5 +17,19 @@ func _process(delta: float) -> void:
 
 
 func _on_slime_timer_timeout() -> void:
-	var slime = slime_scene.instantiate()
-	add_child(slime)
+	for i in enemies:
+		var slime = slime_scene.instantiate()
+		var radians = 2 * PI / enemies * i
+		
+		var vertical = sin(radians)
+		vertical *= randf_range(1.1,1.5)
+		var horizontal = cos(radians)
+		horizontal *= randf_range(1.1,1.5)
+		var spawndir = Vector2(horizontal, vertical)
+		
+		var spawnpos = player.position + spawndir * range
+		#print(spawnpos)
+		
+		slime.position = spawnpos
+		
+		add_child(slime)
